c7da2b47f73edef0324e07ad2a70bc1501780975
Subhamdas20 <subham21.sd@gmail.com>
2021-11-10 23:05:33 +0530

    Added : 2 java program files and 3 txt files

--- src/com/bridgelabz/algorithms/BinarySearch.java
+++ src/com/bridgelabz/algorithms/BinarySearch.java
@@ -1,12 +1,19 @@
 package com.bridgelabz.algorithms;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Arrays;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BinarySearch<T extends Comparable<T>> {
     /**
      * PERFORMS BINARY SEARCH
      *
-     * @param arr to get the array or strings
+     * @param arr              to get the array or strings
      * @param searched_element to search in array
      */
     public int binarySearch(T[] arr, T searched_element) {
@@ -27,19 +34,33 @@ public class BinarySearch<T extends Comparable<T>> {
         return -1;
     }
 
-    public static void main(String[] args) {
-        String[] arr = {"Zimbabwe", "South-Africa", "India", "America", "Yugoslavia", " Australia", "Denmark", "France", "Netherlands", "Italy", "Germany"};
-        String x = "India";
-        Arrays.sort(arr);
-        System.out.println("The sorted array is " + Arrays.toString(arr));
+    public static void main(String[] args) throws IOException {
+        Reader file = new FileReader("E:\\Day16AlgorithmsRpf77\\src\\com\\bridgelabz\\txtfiles\\words.txt");
+        Scanner sc = new Scanner(file);
+//        String st ="";
+        String st[] = new String[11];
+        String words[] = new String[st.length];
+        while (sc.hasNextLine()) {
+
+            st = sc.nextLine().split(" ");
+            for (int i = 0; i < st.length; i++) {
+
+                words[i] = st[i];
+            }
+        }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the element to search");
+        String input = scan.next();
+        Arrays.sort(words);
         BinarySearch<String> search = new BinarySearch<>();
-        int result = search.binarySearch(arr, x);
+        int result = search.binarySearch(words, input);
         if (result == -1)
             System.out.println("Element not present");
-        else
+        else {
             System.out.println("Element found at "
                     + "index " + result);
-
+        }
     }
 }
 
